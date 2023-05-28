472f273f8a46c15c155551c25f663f306e577e9f
Vladislave232 <134703549+Vladislave232@users.noreply.github.com>
2023-05-28 21:03:49 +0300

    Add files via upload

--- 123.lua
+++ 123.lua
@@ -10,8 +10,10 @@ imgui.HotKey = require('imgui_addons').HotKey
 imgui.Spinner = require('imgui_addons').Spinner
 imgui.BufferingBar = require('imgui_addons').BufferingBar
 local razdacha_zapusk = imgui.ImInt(0)
+local sw, sh = getScreenResolution()
 local sampev = require 'lib.samp.events'
 local main_window_state = imgui.ImBool(false)
+local second_window_state = imgui.ImBool(false)
 local dialogArr = {'Наборы', 'Предложить МП', 'Игровой вопрос'}
 local dialogStr = ''
 local dialogArrr = {'Rifa', 'Vagos', 'Aztecas', 'Groove', 'Ballas'}
@@ -22,6 +24,11 @@ local dialogMaff = {'LCN', 'Yakudza', 'Русская Мафия', 'Хитманы'}
 local dialogMaf = ''
 local imBool = imgui.ImBool(false)
 local text_buffer = imgui.ImBuffer(256)
+local texter = imgui.ImBuffer(256)
+local mep = imgui.ImInt(0)
+local mepo = imgui.ImInt(0)
+local prizemep = {u8'"Король Дигла"', u8'"Русская Рулетка"', u8'"Дамба"', u8'"Прятки"', u8'"Последний Выживший"', u8'"Поливалка"', u8'"Бомбардировка"', u8'"Таран"', u8'"PUBG"', u8'"Реакция"', u8'"Бои без правил"', u8'"Гонки"'}
+local prizemp = {u8'"На Выбор"', u8'"Аптечки"', u8'"VIP-CAR"', u8'"Уровень на выбор"', u8'"Стиль Боя"', u8'Номер телефона на выбор', u8'Руководитель пидор', u8'Секрет', u8''} 
 local numbers = {'20+23', '34+78', '46*2' , '4*7', '52+56', '46+54', '20-8', '46-41', '7*8', '44/4' , '10+99', '412-413', '45-54', '7+5', '1+1', '888+111', '111+111'}
 local word = {u8'04', u8'EKB', u8'MS', u8'Healme', u8'Kills', u8'LVL', u8'VIPCAR'}
 local prizeon = {u8'"500 Аптечек"', u8'"Костюм попугая"', u8'"Мигалку на голову"', u8'"Комплект всемогущий"', u8'"Огонек на голову"', u8'"Шляпу курицы"', u8'"Номер телефона на выбор"', u8'"1ккк"', u8'"Стиль Боя"', u8'"500 убийств"', u8'"Уровень"', u8'"ВипКар"', u8'секретный приз'}
@@ -32,9 +39,8 @@ local dlstatus = require('moonloader').download_status
 local inicfg = require 'inicfg'
 update_state = false
 local str_rand = {'123', '123'}
-local script_vers = 12
-local script_vers_text = '1.12'
-
+local script_vers = 15
+local script_vers_text = '1.15'
 local update_url = 'https://raw.githubusercontent.com/Vladislave232/script/main/update.ini'
 local update_path = getWorkingDirectory() .. '/update.ini'
 
@@ -61,13 +67,8 @@ for _, str in ipairs(dialogMaff) do
     dialogMaf = dialogMaf .. str .. "\n"
 end
 
-function sampev.onServerMessage(color, text)
-    lua_thread.create(function()
-        if string.find(text, 'С возвращением в игру, дорогой игрок :)', 1, true) then
-            wait(10000)
-            sampShowDialog(212, "{FFFFFF}О{FF0000}Б{000000}Н{FA8072}О{8B0000}В{FF1493}Л{006400}Е{808000}Н{FF4500}И{FF8C00}Е", "Добавлен цитатник!\n{FFFFFF}Исправлены баги!", "Закрыть", 'Закрыть', 0)
-        end
-    end)
+function obnova(arg)
+    sampShowDialog(212, "{FFFFFF}О{FF0000}Б{000000}Н{FA8072}О{8B0000}В{FF1493}Л{006400}Е{808000}Н{FF4500}И{FF8C00}Е", "Добавлен цитатник!\n{FFFFFF}Исправлены баги!\n{FF00FF}ШОК! Добавлена команда /mep! /rhelp!", "Закрыть", 'Закрыть', 0)
 end
 
 function main()
@@ -77,8 +78,10 @@ function main()
     sampRegisterChatCommand("car", cmd_basa)
     sampRegisterChatCommand('nap', cmd_churka)
     sampRegisterChatCommand("raz", cmd_balalai)
+    sampRegisterChatCommand('obnova', obnova)
     sampRegisterChatCommand('otb', cmd_otbor)
     sampRegisterChatCommand('rhelp', cmd_woopo)
+    sampRegisterChatCommand('mep', cmd_balalai2)
     downloadUrlToFile(update_url, update_path, function(id, status)
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
@@ -94,7 +97,11 @@ function main()
         if update_state then
             downloadUrlToFile(script_url, script_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('{FF0000}Скрипт обновлен успешно')
+                    sampAddChatMessage('{FF0000}[Ekaterinburg-ToolHelper] {00FFFF}ОБНОВЛЕНИЕ! {00FF00}СКОРЕЙ ВВОДИ /OBNOVA')
+                    sampAddChatMessage('{FF0000}[Ekaterinburg-ToolHelper] {00FFFF}ОБНОВЛЕНИЕ! {00FF00}СКОРЕЙ ВВОДИ /OBNOVA')
+                    sampAddChatMessage('{FF0000}[Ekaterinburg-ToolHelper] {00FFFF}ОБНОВЛЕНИЕ! {00FF00}СКОРЕЙ ВВОДИ /OBNOVA')
+                    sampAddChatMessage('{FF0000}[Ekaterinburg-ToolHelper] {00FFFF}ОБНОВЛЕНИЕ! {00FF00}СКОРЕЙ ВВОДИ /OBNOVA')
+                    sampAddChatMessage('{FF0000}[Ekaterinburg-ToolHelper] {00FFFF}ОБНОВЛЕНИЕ! {00FF00}СКОРЕЙ ВВОДИ /OBNOVA')
                     thisScript():reload()
                 end
             end)
@@ -102,7 +109,7 @@ function main()
         local result, button, list, input = sampHasDialogRespond(212)
         if result then
             if button == 1 then
-                sampAddChatMessage('{FF0000}Напишите свой отзыв - @guninik', -1)
+                sampAddChatMessage('{FF0000}Напишите свою реакцию на обновление - @guninik')
             end
         end
         local result, button, list, input = sampHasDialogRespond(13)
@@ -291,54 +298,91 @@ function main()
                 end
             end
         end
-        if main_window_state.v == false then
-        imgui.Process = false
-        end
     end
     wait(2000)
     ran2 = math.random(1, #str_rand)
 end
 
 function imgui.OnDrawFrame()
-    imgui.SetNextWindowPos(imgui.ImVec2(500, 300), imgui.Cond.FirstUseEver)
-    imgui.SetNextWindowSize(imgui.ImVec2(600, 300), imgui.Cond.FirstUseEver)
-    imgui.Begin(u8'Раздача', main_window_state)
-    imgui.Text(u8'Раздача')
-    imgui.Combo(u8'Слово', razdacha, word, #word)
-    imgui.Combo(u8'Призы', razdacha_zapusk, prizeon, #prizeon)
-    imgui.SameLine()
-    imgui.Spinner("##spinner", 5, 3, imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.ButtonHovered]))
-    if imgui.InputText(u8'Введите ID победителя', text_buffer) then
+    if not main_window_state.v and not second_window_state.v then
+        imgui.Process = false
     end
-    if imgui.Button(u8'Раздача') then
-        sampSendChat('/aad РАЗДАЧА | Кто первый напишет "/rep' .. ' ' .. u8:decode(word[razdacha.v + 1]).. '"' .. " тот получит " .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
+    if main_window_state.v then
+        imgui.SetNextWindowPos(imgui.ImVec2(500, 300), imgui.Cond.FirstUseEver)
+        imgui.SetNextWindowSize(imgui.ImVec2(600, 300), imgui.Cond.FirstUseEver)
+        imgui.Begin(u8'Раздача', main_window_state)
+        imgui.Text(u8'Раздача')
+        imgui.Combo(u8'Слово', razdacha, word, #word)
+        imgui.Combo(u8'Призы', razdacha_zapusk, prizeon, #prizeon)
+        imgui.SameLine()
+        imgui.Spinner("##spinner", 5, 3, imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.ButtonHovered]))
+        if imgui.InputText(u8'Введите ID победителя', text_buffer) then
+        end
+        if imgui.Button(u8'Раздача') then
+            sampSendChat('/aad РАЗДАЧА | Кто первый напишет "/rep' .. ' ' .. u8:decode(word[razdacha.v + 1]).. '"' .. " тот получит " .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
+        end
+        imgui.SameLine()
+        if imgui.Button(u8'Выдать') then
+            sampSendChat('/aad РАЗДАЧА | ' .. text_buffer.v .. ' WIN')
+        end
+        imgui.Separator()
+        imgui.Text(u8'Примеры')
+        math.randomseed(os.time())
+        rand = math.random(1, 200)
+        ral = math.random(1, 200)
+        if imgui.Button(u8'Плюс') then
+            sampSendChat('/aad Примеры | Кто первый решит пример ' .. rand .. '+' .. ral .. ' получит ' .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
+        end
+        imgui.SameLine()
+        if imgui.Button(u8'Минус') then
+            sampSendChat('/aad Примеры | Кто первый решит пример ' .. rand .. '-' .. ral .. ' получит ' .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
+        end
+        imgui.SameLine()
+        imgui.BufferingBar("##buffer_bar", 0.7, imgui.ImVec2(390, 6), imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.Button]), imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.ButtonHovered]));
+        imgui.Link('https://vk.com/guninik', u8'Влад Гунинык')
+        imgui.Link('https://vk.com/klagem00n', u8'Сергей Семец')
+        imgui.Link('https://vk.com/natriy14', u8'Помощник: Владислав Дмитров')
+        imgui.Text(u8'Тут скоро появятся цитатники!')
+        imgui.End()
     end
-    imgui.SameLine()
-    if imgui.Button(u8'Выдать') then
-        sampSendChat('/aad РАЗДАЧА | ' .. text_buffer.v .. ' WIN')
+    if second_window_state.v then
+        imgui.SetNextWindowPos(imgui.ImVec2(sw /2, sh /2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+        imgui.SetNextWindowSize(imgui.ImVec2(0, 0), imgui.Cond.FirstUseEver)
+        imgui.Begin(u8'Початковий текст', second_window_state)
+        imgui.Combo(u8'Выберите приз', mep, prizemp, #prizemp)
+        imgui.Combo(u8'Выберите мероприятие', mepo, prizemep, #prizemep)
+        if imgui.Button(u8'Запустить', imgui.ImVec2(390, 120)) then
+            lua_thread.create(function()
+                sampSendChat('aad MP | Уважаемые игроки, сейчас пройдёт мероприятие ' .. u8:decode(prizemep[mepo.v + 1]))
+                wait(1000)
+                sampSendChat('aad MP | Приз: ' .. u8:decode(prizemp[mep.v + 1]) .. '. Для телепорта: /gomp')
+                wait(1000)
+                sampSendChat('/mp')
+            end)
+        end
+        imgui.Separator()
+        imgui.InputText(u8'Кастомный приз', texter)
+        if imgui.Button(u8'Запустить со своим призом', imgui.ImVec2(390, 120)) then
+            lua_thread.create(function()
+                sampSendChat('aad MP | Уважаемые игроки, сейчас пройдёт мероприятие ' .. u8:decode(prizemep[mepo.v + 1]))
+                wait(1000)
+                sampSendChat('aad MP | Приз: ' .. u8:decode(texter.v) .. '! Для телепорта: /gomp')
+                wait(1000)
+                sampSendChat('/mp')
+            end)
+        end
+        imgui.End()
     end
-    imgui.Separator()
-    imgui.Text(u8'Примеры')
-    math.randomseed(os.time())
-    rand = math.random(1, 200)
-    ral = math.random(1, 200)
-    if imgui.Button(u8'Плюс') then
-        sampSendChat('/aad Примеры | Кто первый решит пример ' .. rand .. '+' .. ral .. ' получит ' .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
-    end
-    imgui.SameLine()
-    if imgui.Button(u8'Минус') then
-        sampSendChat('/aad Примеры | Кто первый решит пример ' .. rand .. '-' .. ral .. ' получит ' .. u8:decode(prizeon[razdacha_zapusk.v + 1]))
-    end
-    imgui.SameLine()
-    imgui.BufferingBar("##buffer_bar", 0.7, imgui.ImVec2(390, 6), imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.Button]), imgui.GetColorU32(imgui.GetStyle().Colors[imgui.Col.ButtonHovered]));
-
-    imgui.Text(u8'Тут скоро появятся цитатники!')
-    imgui.End()
 end
 
 function cmd_balalai(arg)
     main_window_state.v = not main_window_state.v
     imgui.Process = main_window_state.v
+end
+
+function cmd_balalai2(arg)
+    second_window_state.v = not second_window_state.vad
+    imgui.Process = second_window_state.v
 end
 
 function cmd_churka(arg)
@@ -369,6 +413,28 @@ end
 
 function cmd_woopo(arg)
     if #arg == 0 then
-        sampShowDialog(209, 'Команды этого скрипта', "\n{FFFFFF}/otb - сделать отбор \n{FF0000}/raz - cделать раздачу \n{00FFFF}/car[id] - выдать машину игроку \n{FF0000}/nap - сделать напоминание", "Выдать", 'Закрыть', 2)
+        sampShowDialog(209, 'Команды этого скрипта', "\n{FFFFFF}/otb - сделать отбор \n{FF0000}/raz - cделать раздачу \n{FF0000}/nap - сделать напоминание \n{FF00FF}/mep - сделать мероприятие \n{00FFFF}/car[id] - выдать машину игроку ", "Выдать", 'Закрыть', 2)
     end
+end
+
+function imgui.Link(link,name,myfunc)
+    myfunc = type(name) == 'boolean' and name or myfunc or false
+    name = type(name) == 'string' and name or type(name) == 'boolean' and link or link
+    local size = imgui.CalcTextSize(name)
+    local p = imgui.GetCursorScreenPos()
+    local p2 = imgui.GetCursorPos()
+    local resultBtn = imgui.InvisibleButton('##'..link..name, size)
+    if resultBtn then
+        if not myfunc then
+            os.execute('explorer '..link)
+        end
+    end
+    imgui.SetCursorPos(p2)
+    if imgui.IsItemHovered() then
+        imgui.TextColored(imgui.ImVec4(0, 0.5, 1, 1), name)
+        imgui.GetWindowDrawList():AddLine(imgui.ImVec2(p.x, p.y + size.y), imgui.ImVec2(p.x + size.x, p.y + size.y), imgui.GetColorU32(imgui.ImVec4(0, 0.5, 1, 1)))
+    else
+        imgui.TextColored(imgui.ImVec4(0, 0.3, 0.8, 1), name)
+    end
+    return resultBtn
 end
